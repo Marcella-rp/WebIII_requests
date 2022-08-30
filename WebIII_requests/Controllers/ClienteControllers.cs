b94a2dc2d08e975ca827fb03dccca03f541ec286
Marcella R Pastore <marcella.rpastore@gmail.com>
2022-08-29 21:45:27 -0300

    Correção das classes

---
+++ WebIII_requests/Controllers/ClienteControllers.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebIII_requests.Services;
+
+namespace WebIII_requests.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClientesController : ControllerBase
+    {
+
+        [HttpGet]
+        public List<Clientes> GetCliente()
+        {
+            return ListaDeClientes.listaDeClientes;
+        }
+
+        [HttpPost]
+        public Clientes AdicionarCliente(Clientes NovoCliente)
+        {
+            ListaDeClientes.listaDeClientes.Add(NovoCliente);
+            return NovoCliente;
+        }
+
+        [HttpPut]
+        public List<Clientes> AlterarCliente(string cpf, string nome)
+        {
+            var listateste = ListaDeClientes.listaDeClientes.Where(c => c.Cpf == cpf).ToList();
+            if (listateste.Exists(c => c.Cpf == cpf))
+            {
+                foreach (var aluno in ListaDeClientes.listaDeClientes)
+                {
+                    aluno.Nome = nome;
+                }
+            }
+            return ListaDeClientes.listaDeClientes;
+        }
+
+        [HttpDelete]
+        public List<Clientes> RemoverCliente(string cpf)
+        {
+            ListaDeAlunos.listaDeAlunos.RemoveAll(c => c.Cpf == cpf);
+            return ListaDeClientes.listaDeClientes;
+        }
+    }
+}
---
+++ WebIII_requests/Services/Clientes.cs
@@ -0,0 +1,29 @@
+﻿namespace WebIII_requests.Services
+{
+    public class Clientes
+    {
+        public string Cpf { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public int Idade { get; set; }
+
+        public Clientes(string cpf, string nome, DateTime datanascimento)
+        {
+            Cpf = cpf;
+            Nome = nome;
+            DataNascimento = datanascimento;
+        }
+
+        public int ObterIdade()
+        {
+            int idade = DateTime.Now.Year - DataNascimento.Year;
+            if (DateTime.Now.DayOfYear < DataNascimento.DayOfYear)
+            {
+                idade--;
+            }
+            return idade;
+        }
+
+    }
+    
+}
